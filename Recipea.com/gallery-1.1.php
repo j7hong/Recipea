a77ff5e98b9d4ba525ffd5ede98dcb09a1744461
j7hong <j7hong@gmail.com>
2014-02-08 19:23:07 -0500

    Coded public homepage
    Styled but need to add styling:
    - link buttons in navigation (header)
    - buttons in navigation (header)
    - positioning of drop-down lists in search boxes (search)
    - page right button not displaying properly (footer)

--- Recipea.com/gallery-1.1.php
+++ Recipea.com/gallery-1.1.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <title>Recipes | PHP project - Team PHP</title>
         <meta name="description" content="Homepage for Recipes.com - an community recipe sharing site.">
-        <link rel="stylesheet" href="css/styles-1-0.css" /> 
+
         <!--[if lt IE 9]>
           <script src="//html5shim.googlecode.com/svn/trunk/html5.js"></script>
         <![endif]-->
@@ -13,162 +13,222 @@
         <h1 class="invisible">Gallery</h1>
         <section>
             <h2 class="invisible">Deserts</h2>
-            <section>
-                <h3 class="invisible">Desert title</h3>
-                <div>
+
+            <section>  <!-- Desert title-->
+
+                <div class="margin-bottom-s">
                     <img src="img/gallery/green-bubble.png" alt="green bubble icon" />
-                    <span>Desserts</span>
+                    <span class="heading-ml2 uppercase text-colour-3">Desserts</span>
                     <img src="img/gallery/green-bubble.png" alt="green bubble icon" />
                 </div>
+
             </section>
 
             <!--        Recipe T-1-->
-            <section>
+            <section class='float-left margin-right-m'>
+                <h3 class="invisible">Chocolate Cookie</h3>
                 <div>
-                    <img src="img/gallery/orange-frame-small-top.png" alt="orange frame small top" />
-                    <div><span>Chocolate Cookie</span></div>
+<!--                    <img src="img/gallery/orange-frame-small-top.png" alt="orange frame small top" />-->
+                    <div class="background-colour1 rounded-frame-top">
+                        <span class="heading-sm text-colour1 indent-s2 vertical-align-frame-text">Chocolate Cookie</span>
+                    </div>
+
                     <img src="img/gallery/food/desserts/chocolate-cookie.png" alt="chocolate-cookie" />
-                    <img src="img/gallery/orange-frame-small-bottom.png" alt="orange frame small bottom" />
-                    <section>
-                        <img src="img/gallery/piggy-bank-icon.png" alt="piggy-bank-icon" />
-                        <img src="img/gallery/timer-icon.png" alt="timer-icon" />
-                        <img src="img/gallery/scale-icon.png" alt="scale-icon" />
-                        <img src="img/gallery/heart-icon.png" alt="heart-icon" />
-                    </section>
+
+                    <div class="background-colour1 rounded-frame-bottom">
+                        <section>
+                            <img class="indent-s2" src="img/gallery/piggy-bank-icon.png" alt="piggy-bank-icon" />
+                            <img src="img/gallery/timer-icon.png" alt="timer-icon" />
+                            <img src="img/gallery/scale-icon.png" alt="scale-icon" />
+                            <img src="img/gallery/heart-icon.png" alt="heart-icon" />
+                        </section>
+                    </div>
+<!--                    <img src="img/gallery/orange-frame-small-bottom.png" alt="orange frame small bottom" />-->
+
                 </div>
-            </section>
+            </section> <!-- /small box-->
 
             <!--        Recipe T-2-->
-            <section>
+            <section class='float-left margin-right-m'>
+                <h3 class="invisible">Caramel Muffin</h3>
                 <div>
-                    <img src="img/gallery/orange-frame-small-top.png" alt="orange frame small top" />
-                    <div><span>Caramel Muffin</span></div>
+
+                    <div class="background-colour1 rounded-frame-top">
+                        <span class="heading-sm text-colour1 indent-s2 vertical-align-frame-text">Caramel Muffin</span>
+                    </div>
+
                     <img src="img/gallery/food/desserts/caramel-muffin.png" alt="caramel-muffin" />
-                    <img src="img/gallery/orange-frame-small-bottom.png" alt="orange frame small bottom" />
-                    <section>
-                        <img src="img/gallery/piggy-bank-icon.png" alt="piggy-bank-icon" />
-                        <img src="img/gallery/timer-icon.png" alt="timer-icon" />
-                        <img src="img/gallery/scale-icon.png" alt="scale-icon" />
-                        <img src="img/gallery/heart-icon.png" alt="heart-icon" />
-                    </section>
+
+                    <div class="background-colour1 rounded-frame-bottom">
+                        <section>
+                            <img class="indent-s2" src="img/gallery/piggy-bank-icon.png" alt="piggy-bank-icon" />
+                            <img src="img/gallery/timer-icon.png" alt="timer-icon" />
+                            <img src="img/gallery/scale-icon.png" alt="scale-icon" />
+                            <img src="img/gallery/heart-icon.png" alt="heart-icon" />
+                        </section>
+                    </div>
+
                 </div>
-            </section>
+            </section> <!-- /small box-->
 
             <!--        Recipe T-3-->
-            <section>
+            <section class='float-left margin-right-m'>
+                <h3 class="invisible">Cookie Cheese Cake</h3>
                 <div>
-                    <img src="img/gallery/orange-frame-small-top.png" alt="orange frame small top" />
-                    <div><span>Cookie Cheese Cake</span></div>
+
+                    <div class="background-colour1 rounded-frame-top">
+                        <span class="heading-sm text-colour1 indent-s2 vertical-align-frame-text">Cookie Cheese Cake</span>
+                    </div>
+
                     <img src="img/gallery/food/desserts/coffee-cheese-cake.png" alt="coffee-cheese-cake" />
-                    <img src="img/gallery/orange-frame-small-bottom.png" alt="orange frame small bottom" />
-                    <section>
-                        <img src="img/gallery/piggy-bank-icon.png" alt="piggy-bank-icon" />
-                        <img src="img/gallery/timer-icon.png" alt="timer-icon" />
-                        <img src="img/gallery/scale-icon.png" alt="scale-icon" />
-                        <img src="img/gallery/heart-icon.png" alt="heart-icon" />
-                    </section>
+
+                    <div class="background-colour1 rounded-frame-bottom">
+                        <section>
+                            <img class="indent-s2" src="img/gallery/piggy-bank-icon.png" alt="piggy-bank-icon" />
+                            <img src="img/gallery/timer-icon.png" alt="timer-icon" />
+                            <img src="img/gallery/scale-icon.png" alt="scale-icon" />
+                            <img src="img/gallery/heart-icon.png" alt="heart-icon" />
+                        </section>
+                    </div>
+
                 </div>
-            </section>
+            </section> <!-- /small box-->
 
             <!--        Recipe T-4 - Large-->
-            <section>
+            <section class='float-left'>
+                <h3 class="invisible">Raspberry Parfait</h3>
                 <div>
-                    <img src="img/gallery/orange-frame-large-top.png" alt="orange-frame-large-top" />
-                    <div><span>Raspberry Parfait</span></div>
+
+                    <div class="background-colour1 rounded-frame-top-l">
+                        <span class="heading-sm text-colour1 indent-s2 vertical-align-frame-text">Raspberry Parfait</span>
+                    </div>
+
                     <img src="img/gallery/food/desserts/raspberry-parfait.png" alt="raspberry-parfait" />
-                    <img src="img/gallery/ribbon.png" alt="ribbon" />
-                    <img src="img/gallery/orange-frame-large-bottom.png" alt="orange-frame-large-bottom" />
-                    <section>
-                        <img src="img/gallery/piggy-bank-icon.png" alt="piggy-bank-icon" />
-                        <img src="img/gallery/timer-icon.png" alt="timer-icon" />
-                        <img src="img/gallery/scale-icon.png" alt="scale-icon" />
-                        <img src="img/gallery/heart-icon.png" alt="heart-icon" />
-                    </section>
+
+                    <div class="background-colour1 rounded-frame-bottom-l">
+                        <section>
+                            <img class="indent-s2" src="img/gallery/piggy-bank-icon.png" alt="piggy-bank-icon" />
+                            <img src="img/gallery/timer-icon.png" alt="timer-icon" />
+                            <img src="img/gallery/scale-icon.png" alt="scale-icon" />
+                            <img src="img/gallery/heart-icon.png" alt="heart-icon" />
+                        </section>
+                    </div>
+
                 </div>
-            </section>
+            </section> <!-- /large box-->
+            
+        </section> <!-- /Desserts section-->
 
-        </section>
-
-        <img src="img/gallery/dotted-line-long.png" al="dotted-line-long" />
+        <img class="margin-top-m3 margin-bottom-m" src="img/gallery/dotted-line-long.png" al="dotted-line-long" />
 
         <section>
             <h2 class="invisible">Noodles & Pasta</h2>
-            <section>
-                <h3 class="invisible">Noodles & Pasta Title</h3>
-                <div>
+
+            <section>  <!-- Noodles & Pasta title-->
+
+                <div class="margin-bottom-s">
                     <img src="img/gallery/green-bubble.png" alt="green bubble icon" />
-                    <span>Noodles & Pasta</span>
+                    <span class="heading-ml2 uppercase text-colour-3">Noodles & Pasta</span>
                     <img src="img/gallery/green-bubble.png" alt="green bubble icon" />
                 </div>
+
             </section>
 
             <!--        Recipe B-1-->
-            <section>
+            <section class='float-left margin-right-m'>
+                <h3 class="invisible">Pad Thai</h3>
                 <div>
-                    <img src="img/gallery/orange-frame-small-top.png" alt="orange frame small top" />
-                    <div><span>Pad Thai</span></div>
-                    <img src="img/gallery/food/noodles-pasta/pad-thai.png" alt="pad-thai" />
-                    <img src="img/gallery/orange-frame-small-bottom.png" alt="orange frame small bottom" />
-                    <section>
-                        <img src="img/gallery/piggy-bank-icon.png" alt="piggy-bank-icon" />
-                        <img src="img/gallery/timer-icon.png" alt="timer-icon" />
-                        <img src="img/gallery/scale-icon.png" alt="scale-icon" />
-                        <img src="img/gallery/heart-icon.png" alt="heart-icon" />
-                    </section>
-                </div>
-            </section>
+<!--                    <img src="img/gallery/orange-frame-small-top.png" alt="orange frame small top" />-->
+                    <div class="background-colour1 rounded-frame-top">
+                        <span class="heading-sm text-colour1 indent-s2 vertical-align-frame-text">Pad Thai</span>
+                    </div>
 
-            <!--        Recipe B-2 - Large-->
-            <section>
-                <div>
-                    <img src="img/gallery/orange-frame-large-top.png" alt="orange-frame-large-top" />
-                    <div><span>Lemon Parmesan Pasta</span></div>
-                    <img src="img/gallery/food/noodles-pasta/lemon-parmasan-pasta.png" alt="lemon-parmasan-pasta" />
-                    <img src="img/gallery/ribbon.png" alt="ribbon" />
-                    <img src="img/gallery/orange-frame-large-bottom.png" alt="orange-frame-large-bottom" />
-                    <section>
-                        <img src="img/gallery/piggy-bank-icon.png" alt="piggy-bank-icon" />
-                        <img src="img/gallery/timer-icon.png" alt="timer-icon" />
-                        <img src="img/gallery/scale-icon.png" alt="scale-icon" />
-                        <img src="img/gallery/heart-icon.png" alt="heart-icon" />
-                    </section>
+                    <img src="img/gallery/food/noodles-pasta/pad-thai.png" alt="pad-thai" />
+
+                    <div class="background-colour1 rounded-frame-bottom">
+                        <section>
+                            <img class="indent-s2" src="img/gallery/piggy-bank-icon.png" alt="piggy-bank-icon" />
+                            <img src="img/gallery/timer-icon.png" alt="timer-icon" />
+                            <img src="img/gallery/scale-icon.png" alt="scale-icon" />
+                            <img src="img/gallery/heart-icon.png" alt="heart-icon" />
+                        </section>
+                    </div>
+<!--                    <img src="img/gallery/orange-frame-small-bottom.png" alt="orange frame small bottom" />-->
+
                 </div>
-            </section>
+            </section> <!-- /small box-->
+
+            <!--        Recipe B-2  - Large-->
+            <section class='float-left margin-right-m'>
+                <h3 class="invisible">Lemon Parmesan Pasta</h3>
+                <div>
+
+                    <div class="background-colour1 rounded-frame-top-l">
+                        <span class="heading-sm text-colour1 indent-s2 vertical-align-frame-text">Lemon Parmesan Pasta</span>
+                    </div>
+
+                    <img src="img/gallery/food/noodles-pasta/lemon-parmasan-pasta.png" alt="lemon-parmasan-pasta" />
+
+                    <div class="background-colour1 rounded-frame-bottom-l">
+                        <section>
+                            <img class="indent-s2" src="img/gallery/piggy-bank-icon.png" alt="piggy-bank-icon" />
+                            <img src="img/gallery/timer-icon.png" alt="timer-icon" />
+                            <img src="img/gallery/scale-icon.png" alt="scale-icon" />
+                            <img src="img/gallery/heart-icon.png" alt="heart-icon" />
+                        </section>
+                    </div>
+
+                </div>
+            </section> <!-- /large box-->
 
             <!--        Recipe B-3-->
-            <section>
+            <section class='float-left margin-right-m'>
+                <h3 class="invisible">Tonkotsu Ramen</h3>
                 <div>
-                    <img src="img/gallery/orange-frame-small-top.png" alt="orange frame small top" />
-                    <div><span>Tonkotsu Ramen</span></div>
+
+                    <div class="background-colour1 rounded-frame-top">
+                        <span class="heading-sm text-colour1 indent-s2 vertical-align-frame-text">Tonkotsu Ramen</span>
+                    </div>
+
                     <img src="img/gallery/food/noodles-pasta/tonkotsu-ramen.png" alt="tonkotsu-ramen" />
-                    <img src="img/gallery/orange-frame-small-bottom.png" alt="orange frame small bottom" />
-                    <section>
-                        <img src="img/gallery/piggy-bank-icon.png" alt="piggy-bank-icon" />
-                        <img src="img/gallery/timer-icon.png" alt="timer-icon" />
-                        <img src="img/gallery/scale-icon.png" alt="scale-icon" />
-                        <img src="img/gallery/heart-icon.png" alt="heart-icon" />
-                    </section>
-                </div>
-            </section>
 
-            <!--        Recipe B-4-->
-            <section>
+                    <div class="background-colour1 rounded-frame-bottom">
+                        <section>
+                            <img class="indent-s2" src="img/gallery/piggy-bank-icon.png" alt="piggy-bank-icon" />
+                            <img src="img/gallery/timer-icon.png" alt="timer-icon" />
+                            <img src="img/gallery/scale-icon.png" alt="scale-icon" />
+                            <img src="img/gallery/heart-icon.png" alt="heart-icon" />
+                        </section>
+                    </div>
+
+                </div>
+            </section> <!-- /small box-->
+
+            <!--        Recipe B-4 -->
+            <section class='float-left margin-right-m'>
+                <h3 class="invisible">Mexican Lasagna</h3>
                 <div>
-                    <img src="img/gallery/orange-frame-small-top.png" alt="orange frame small top" />
-                    <div><span>Mexican Lasagna</span></div>
+
+                    <div class="background-colour1 rounded-frame-top">
+                        <span class="heading-sm text-colour1 indent-s2 vertical-align-frame-text">Mexican Lasagna</span>
+                    </div>
+
                     <img src="img/gallery/food/noodles-pasta/mexican-lasagna.png" alt="mexican-lasagna" />
-                    <img src="img/gallery/orange-frame-small-bottom.png" alt="orange frame small bottom" />
-                    <section>
-                        <img src="img/gallery/piggy-bank-icon.png" alt="piggy-bank-icon" />
-                        <img src="img/gallery/timer-icon.png" alt="timer-icon" />
-                        <img src="img/gallery/scale-icon.png" alt="scale-icon" />
-                        <img src="img/gallery/heart-icon.png" alt="heart-icon" />
-                    </section>
+
+                    <div class="background-colour1 rounded-frame-bottom">
+                        <section>
+                            <img class="indent-s2" src="img/gallery/piggy-bank-icon.png" alt="piggy-bank-icon" />
+                            <img src="img/gallery/timer-icon.png" alt="timer-icon" />
+                            <img src="img/gallery/scale-icon.png" alt="scale-icon" />
+                            <img src="img/gallery/heart-icon.png" alt="heart-icon" />
+                        </section>
+                    </div>
+
                 </div>
-            </section>
-
-        </section>
-
+            </section> <!-- /small box-->
+            
+        </section> <!-- /Noodles and pasta section-->
+            
     </div> <!--/container-->
 </body>
 </html>
--- Recipea.com/index-public-1.0.0.php
+++ Recipea.com/index-public-1.0.0.php
@@ -5,11 +5,11 @@
         <title>Recipes | PHP project - Team PHP </title>
         <meta name="description" content="Homepage for Recipes.com - an community recipe sharing site.">
         <link rel="stylesheet" href="css/styles-1-8.css" />
-        <link rel="stylesheet" href="css/search-public-1.0.0.css" />
+
 <!--        
         <link rel="stylesheet" href="css/header-1.0.0.css" />
         <link rel="stylesheet" href="css/search-public-1.0.0.css" />
-        <link rel="stylesheet" href="css/gallery-1.0.0.css" />
+        <link rel="stylesheet" href="css/gallery-1.0.0.css" />  didn't use yet
         <link rel="stylesheet" href="css/footer-1.0.0.css" />-->
     </head>
     <body>
@@ -28,13 +28,13 @@
                 echo '<div class="main-content">';
                 // <!--- - - - - - - - -  SEARCH - - - - - - - - ---> 
 
-                echo '<div class="main-content">';
+                echo '<div class="main-content red-border">';
                 require_once 'search-1.4.3.php';                
                 echo '</div>';
 
                 // <!--- - - - - - - - -  GALLERY - - - - - - - - ---> 
 
-                echo '<div class="">';
+                echo '<div class="margin-top-m3">';
                 echo '<div class="clear"></div>';
                 require_once 'gallery-1.1.php';
                 echo '</div>';
