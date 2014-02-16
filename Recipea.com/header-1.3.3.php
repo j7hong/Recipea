606bd82ad1fc117ef2d7f829870a746cbc582229
j7hong <j7hong@gmail.com>
2014-02-15 19:12:47 -0500

    Header - Mostly responsive
    - Responsive except for buttons
    - Don't know what's happening there

--- Recipea.com/header-1.3.3.php
+++ Recipea.com/header-1.3.3.php
@@ -1,91 +1,163 @@
-<?php
-// arrays
-// main navigation in header
-//$navigation_links = array("home", "recipes", "events", "media", "games", "about", "contact", "careers");
-//$navigation_link_names = array("About", "Recipes", "Events", "Media", "Games", "About", "Contact", "Careers");
-//
-//// navigation links in footer
-//$footer_links = array("sitemap", "recipes", "events", "media", "games", "about", "contact", "careers");
-//$footer_link_names = array("Sitemap", "Recipes", "Events", "Media", "Games", "About", "Contact", "Careers");
-//
-//function create_link($index, $href_array, $label_array) {
-//    $link = '<li><a href=' . $href_array[$index] . '.php>' . $label_array[$index] . '</a></li>';
-//    return $link;
-//}
-//
-//function display_navigation($links_array, $links_names_array) {
-//
-//    if (count($links_array) == count($links_names_array)) {
-//
-//        for ($i = 0; $i < count($links_array); $i++) {
-//            echo create_link($i, $links_array, $links_names_array);
-//        }
-//    } else {
-//        echo "The number of links does not match the number of link names.";
-//    }
-//}
-?>
+<!--- - - - - - - - -  HEADER  - - - - - - - - ---> 
+        <link href="css/bootstrap.min.css" rel="stylesheet">
+        <link href="css/styles-1-8.css" rel="stylesheet" />
+<!--<div class="row">
 
-<!DOCTYPE html>
-<html lang="en">
-    <head>
-        <meta charset="utf-8">
-        <title>Recipes | PHP project - Team PHP</title>
-        <meta name="description" content="Homepage for Recipes.com - an community recipe sharing site.">
-        <link rel="stylesheet" href="css/header-1.0.0.css" /> 
-        <!--[if lt IE 9]>
-          <script src="//html5shim.googlecode.com/svn/trunk/html5.js"></script>
-        <![endif]-->
-    </head>
-    <body>
-        
-        <!--- - - - - - - - -  HEADER  - - - - - - - - ---> 
-        
-        
-        <header class="page-header">                
-            <a href="index-1.1.php"><img class="float-left" src="img/header/logo.png" alt="Recipea logo" /></a>
+    <header class="page-header">     
 
+        <div class="col-md-2 col-sm-2 col-xs-11 col-xs-offset-1">
+            <a href="index-1.1.php"><img src="img/header/logo.png" alt="Recipea logo" /></a>
+        </div>
+
+
+        <div class="col-md-6 col-sm-6 col-xs-12">
             <nav role="navigation" class="main-nav centre-align">
-                <div class="float-left">
-                    <a href="#"><img src="img/header/main-nav/home-icon.png" alt="home logo" /></a>
-                    <span class="uppercase">Home</span>
-                </div>
-                <div class="float-left">
-                    <a href="#"><img src="img/header/main-nav/recipes-icon.png" alt="recipes logo" /></a>
-                    <span class="uppercase">Recipes</span>
-                </div>
-                <div class="float-left">
-                    <a href="#"><img src="img/header/main-nav/events-icon.png" alt="events logo" /></a>
-                    <span class="uppercase">Events</span>
-                </div>
-                <div class="float-left">
-                    <a href="#"><img src="img/header/main-nav/media-icon.png" alt="media logo" /></a>
-                    <span class="uppercase">Media</span>
-                </div>
-                <div class="float-left">
-                    <a href="#"><img src="img/header/main-nav/games-icon.png" alt="games logo" /></a>
-                    <span class="uppercase">Games</span>
-                </div>
-                <div class="float-left">
-                    <a href="#"> <img src="img/header/main-nav/about-icon.png" alt="about logo" /></a>
-                    <span class="uppercase">About</span>
-                </div>
-                <div class="float-left">
-                    <a href="#"><img src="img/header/main-nav/contact-icon.png" alt="contact logo" /></a>
-                    <span class="uppercase">Contact</span>
+
+                <div class="col-md-2 col-sm-2 col-xs-12">
+                    <div class="col-md-2 col-sm-2 col-xs-12">
+                        <a href="#"><img src="img/header/main-nav/home-icon.png" alt="home logo" /></a>
+                    </div>
+                    <div class="col-md-2 col-sm-2 col-xs-12 clear">
+                        <span class="uppercase">Home</span>
+                    </div>                                        
                 </div>
 
-            </nav>
+                <div class="col-md-2 col-sm-12 col-xs-12">
+                    <div class="col-md-2 col-sm-12 col-xs-12">
+                        <a href="#"><img src="img/header/main-nav/recipes-icon.png" alt="recipes logo" /></a>
+                    </div>
+                    <div class="col-md-2 col-sm-12 col-xs-12 clear">
+                        <span class="uppercase">Recipes</span>
+                    </div>  
+                </div>
 
-            <!--            <nav role="navigation" class="main-nav"> 
-            <? // php display_navigation($navigation_links, $navigation_link_names); ?>
-                        </nav>-->
+                <div class="col-md-2 col-sm-2 col-xs-12">
+                    <div class="col-md-2 col-sm-2 col-xs-12">
+                        <a href="#"><img src="img/header/main-nav/recipes-icon.png" alt="recipes logo" /></a>
+                    </div>
+                    <div class="col-md-2 col-sm-2 col-xs-12 clear">
+                        <span class="uppercase">Recipes</span>
+                    </div>  
+                </div>
 
-            <div class=" float-left">
-                <a href="#"><img src="img/header/btn-post-recipe.png" alt="post a recipe button" class="margin-right-s red-border" /></a>
-                <a href="#"><img src="img/header/btn-login.png" alt="login button" class="red-border" /></a>
-            </div>
 
-        </header>                  
-    </body>
-</html>
+                <a href="#"><img src="img/header/main-nav/events-icon.png" alt="events logo" /></a>
+                <span class="uppercase">Events</span>
+
+                <div class="col-md-2 col-sm-2 col-xs-12">
+                    <div class="col-md-2 col-sm-2 col-xs-12">
+                        <a href="#"><img src="img/header/main-nav/recipes-icon.png" alt="recipes logo" /></a>
+                    </div>
+                    <div class="col-md-2 col-sm-2 col-xs-12 clear">
+                        <span class="uppercase">Recipes</span>
+                    </div>  
+                </div>
+
+
+
+                <a href="#"><img src="img/header/main-nav/media-icon.png" alt="media logo" /></a>
+                <span class="uppercase">Media</span>
+
+                <div class="col-md-2 col-sm-2 col-xs-12">
+                    <div class="col-md-2 col-sm-2 col-xs-12">
+                        <a href="#"><img src="img/header/main-nav/recipes-icon.png" alt="recipes logo" /></a>
+                    </div>
+                    <div class="col-md-2 col-sm-2 col-xs-12 clear">
+                        <span class="uppercase">Recipes</span>
+                    </div>  
+                </div>
+
+
+                <a href="#"><img src="img/header/main-nav/games-icon.png" alt="games logo" /></a>
+                <span class="uppercase">Games</span>
+
+                <div class="col-md-2 col-sm-2 col-xs-12">
+                    <div class="col-md-2 col-sm-2 col-xs-12">
+                        <a href="#"><img src="img/header/main-nav/recipes-icon.png" alt="recipes logo" /></a>
+                    </div>
+                    <div class="col-md-2 col-sm-2 col-xs-12 clear">
+                        <span class="uppercase">Recipes</span>
+                    </div>  
+                </div>
+
+                <a href="#"> <img src="img/header/main-nav/about-icon.png" alt="about logo" /></a>
+                <span class="uppercase">About</span>
+
+                <div class="col-md-2 col-sm-2 col-xs-12">
+                    <div class="col-md-2 col-sm-2 col-xs-12">
+                        <a href="#"><img src="img/header/main-nav/recipes-icon.png" alt="recipes logo" /></a>
+                    </div>
+                    <div class="col-md-2 col-sm-2 col-xs-12 clear">
+                        <span class="uppercase">Recipes</span>
+                    </div>  
+                </div>
+
+                <a href="#"><img src="img/header/main-nav/contact-icon.png" alt="contact logo" /></a>
+                <span class="uppercase">Contact</span>
+
+        </div>
+
+      
+
+        </nav>
+</div>-->
+
+<div class="row">
+
+    <header class="page-header">     
+
+        <div class="col-md-2"> <!--col-sm-2 col-xs-11 col-xs-offset-1-->
+            <a href="index-1.1.php"><img src="img/header/logo.png" alt="Recipea logo" /></a>
+        </div>
+
+  <div class="navbar-header navbar-default">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#collapse">
+                <span class="sr-only">Toggle Navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+
+        </div>
+
+        <div class="collapse navbar-collapse col-md-6" id="collapse">
+            <ul class="nav navbar-nav">
+                <li>
+                    <a href="#"><img src="img/header/main-nav/recipes-icon.png" alt="recipes logo" />Home</a>
+                </li>
+                <li>
+                    <a href="#"><img src="img/header/main-nav/recipes-icon.png" alt="recipes logo" />Recipes</a>
+                </li>
+                <li>
+                    <a href="#"><img src="img/header/main-nav/recipes-icon.png" alt="recipes logo" />Events</a>
+                </li>
+                <li>
+                    <a href="#"><img src="img/header/main-nav/recipes-icon.png" alt="recipes logo" />Media</a>
+                </li>
+                <li>
+                    <a href="#"><img src="img/header/main-nav/recipes-icon.png" alt="recipes logo" />Games</a>
+                </li>
+                <li>
+                    <a href="#"><img src="img/header/main-nav/recipes-icon.png" alt="recipes logo" />About</a>
+                </li>
+                <li>
+                    <a href="#"><img src="img/header/main-nav/recipes-icon.png" alt="recipes logo" />Contact Us</a>
+                </li>
+            </ul>
+        </div>
+        
+<div class="col-md-1">
+        <a href="#"><img src="img/header/btn-post-recipe.png" alt="post a recipe button" /></a>                
+</div>
+
+<div class="col-md-1">
+        <a href="#"><img src="img/header/btn-login.png" alt="login button" /></a>
+</div>
+
+
+</header>    
+
+</div> <!-- /row for header-->
+
+<!--    </body>
+</html>-->
--- Recipea.com/versions/index-public-1.1.0.php
+++ Recipea.com/versions/index-public-1.1.0.php
@@ -7,10 +7,10 @@
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        
+        <link rel="stylesheet" href="css/styles-1-8.css" />
+
         <!-- Bootstrap -->
         <link href="css/bootstrap.min.css" rel="stylesheet">
-        <link href="css/styles-1-8.css" rel="stylesheet" />
 
         <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
         <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
@@ -18,7 +18,7 @@
           <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
           <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
         <![endif]-->
-
+        
         <!--        
                 <link rel="stylesheet" href="css/header-1.0.0.css" />
                 <link rel="stylesheet" href="css/search-public-1.0.0.css" />
@@ -63,9 +63,5 @@
         require_once 'footer-1.2.4.php';
         ?>
 
-        <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-        <script src="https://code.jquery.com/jquery.js"></script>
-        <!-- Include all compiled plugins (below), or include individual files as needed -->
-        <script src="js/bootstrap.min.js"></script>
     </body>
 </html>
